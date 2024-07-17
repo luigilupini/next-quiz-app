fe1626a62f968059d7d76e887d33810091e8aeee
Luigi Lupini <luigi.lupini@gmail.com>
2024-07-17 22:01:08 +0200

    refactor: Update imports for Slider component, add styles for Carousel component, and remove unused code

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    // domains: ["uploadthing.com", "utfs.io"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.artic.edu",
-        port: "",
-        pathname: "/iiif/2/**",
-      },
-    ],
-  },
   logging: {
     fetches: {
       fullUrl: true,
